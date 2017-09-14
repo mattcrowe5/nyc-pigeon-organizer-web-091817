fa310645d7ce4099c2a538cbcab0f53f2f6da316
Matt Crowe <crowe.matthew5@gmail.com>
2017-09-14 19:32:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  data.each do |category, hash|
+    hash.each do |key, array|
+      array.each do |name|
+          pigeons[name] = {} unless pigeons[name]
+          pigeons[name][category] = [] unless pigeons[name][category]
+          pigeons[name][category].push(key.to_s)
+        end
+      end
+    end
+  pigeons
 end
